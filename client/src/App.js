5b0b05987bced3a194c334a1791b2a89b0f1ab88
Ritik Parmar <ritikparmar255@gmail.com>
2025-04-26 23:42:51 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ function App() {
         const address = await signer.getAddress();
         setAccount(address);
         // let contractAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
-        let contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
         const contract = new ethers.Contract(
           contractAddress,
